b8fc4866e53454ab2bfbd863aeaa0d6212943022
Sandeep Kumar Gupta <sandeepkgupta007@gmail.com>
2019-08-23 21:24:43 +0530

    install: ngxs done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxsModule} from '@ngxs/store';
+import {AppState} from './state/app.state';
+import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
+import {environment} from '../environments/environment';
+import {NgxsStoragePluginModule} from '@ngxs/storage-plugin';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([
+      AppState
+    ]),
+    NgxsStoragePluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot({disabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
